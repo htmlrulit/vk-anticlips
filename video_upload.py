5753d29984925580ee3ab8c9c72348128e0fc51a
htmlrulit <34247867+htmlrulit@users.noreply.github.com>
2024-07-06 14:01:46 +0300

    Update video_upload.py
    Добавлен параметр privacy_view

--- video_upload.py
+++ video_upload.py
@@ -10,11 +10,19 @@ def upload_videos_to_vk_folder(vk_session, folder_path, group_id, output_file):
     for filename in os.listdir(folder_path):
         if filename.endswith(".mp4"):
             file_path = os.path.join(folder_path, filename)
-            new_title = f"Название видео"  # название для видео
+            new_title = f"Название видео"  # Название для видео
             print(f'Uploading {filename} as {new_title}...')
 
             try:
-                video_info = vk.video.save(name=new_title, group_id=group_id, is_private=0)  # (0 - публичное, 1 - приватное)
+                # Создаем запись для видео в VK с установленной приватностью просмотра
+                video_info = vk.video.save(
+                    name=new_title, 
+                    group_id=group_id, 
+                    is_private=0,  # 0 - публичное, 1 - приватное
+                    privacy_view="members"  # Приватность просмотра: доступно участникам
+                )
+                
+                # Загружаем видео по полученному URL
                 with open(file_path, 'rb') as video_file:
                     files = {'video_file': video_file}
                     response = requests.post(video_info['upload_url'], files=files)
@@ -37,9 +45,9 @@ def upload_videos_to_vk_folder(vk_session, folder_path, group_id, output_file):
     print(f'Links to uploaded videos have been saved to {output_file}')
 
 if __name__ == "__main__":
-    vk_session = vk_api.VkApi(token='') # токен VK Admin https://vkhost.github.io
-    folder_path = '' # папка с видео
-    group_id = '' # id группы (положительное)
-    output_file = 'uploaded_videos.json'  # файл с ссылками на видео
+    vk_session = vk_api.VkApi(token='')  # Укажите ваш токен доступа
+    folder_path = ''  # Укажите путь к папке с видео
+    group_id = ''  # Укажите ID вашей группы
+    output_file = 'uploaded_videos.json'  # Укажите путь к выходному файлу
 
     upload_videos_to_vk_folder(vk_session, folder_path, group_id, output_file)
